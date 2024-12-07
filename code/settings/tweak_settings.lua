fe3a61c1ef904e3d07dc86ee46fe5aabbd62e31a
Stephen Barnes <stephen6174@gmail.com>
2024-12-08 00:45:37 +0200

    Fix Ocean Dumping settings

--- code/settings/tweak_settings.lua
+++ code/settings/tweak_settings.lua
@@ -8,7 +8,7 @@ u.setDefaultOrForce("PowerMultiplier-heating", "double", 2)
 u.setDefaultOrForce("PowerMultiplier-solar", "double", 1)
 
 -- Set options for Ocean Dumping mod.
-u.setDefaultOrForce("oceandump-enable-nauvis-dumping", "bool", false)
-u.setDefaultOrForce("oceandump-enable-gleba-dumping", "bool", false)
-u.setDefaultOrForce("oceandump-enable-fulgora-dumping", "bool", true)
-u.setDefaultOrForce("oceandump-enable-aquilo-dumping", "bool", true)
+u.setDefaultOrForce("enable-nauvis-dumping", "bool", false)
+u.setDefaultOrForce("enable-gleba-dumping", "bool", false)
+u.setDefaultOrForce("enable-fulgora-dumping", "bool", true)
+u.setDefaultOrForce("enable-aquilo-dumping", "bool", true)
