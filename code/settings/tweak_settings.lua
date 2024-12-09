f3ed25ec40fda016034a8afaf1129f0faee6aebd
Stephen Barnes <stephen6174@gmail.com>
2024-12-09 11:57:00 +0200

    Added Diurnal Dynamics mod (longer darker nights)

--- code/settings/tweak_settings.lua
+++ code/settings/tweak_settings.lua
@@ -12,3 +12,15 @@ u.setDefaultOrForce("enable-nauvis-dumping", "bool", false)
 u.setDefaultOrForce("enable-gleba-dumping", "bool", false)
 u.setDefaultOrForce("enable-fulgora-dumping", "bool", true)
 u.setDefaultOrForce("enable-aquilo-dumping", "bool", true)
+
+-- Set options for the Diurnal Dynamics mod.
+u.setDefaultOrForce("data-dd-dark-perc", 100.)
+u.setDefaultOrForce("data-dd-dark-platform", true)
+u.setDefaultOrForce("data-dd-diurnal-cycle-mult", 4)
+u.setDefaultOrForce("data-dd-accum-mult", 1)
+u.setDefaultOrForce("data-dd-night-only", true) -- Attacks only at night
+u.setDefaultOrForce("data-dd-night-mode", false) -- Not always night
+u.setDefaultOrForce("data-dd-dusk", 0.25)
+u.setDefaultOrForce("data-dd-evening", 0.35) -- Adjusted to be earlier, 0.45->0.35, for longer night.
+u.setDefaultOrForce("data-dd-morning", 0.65) -- Adjusted to be later, 0.55->0.65, for longer night.
+u.setDefaultOrForce("data-dd-dawn", 0.75)
