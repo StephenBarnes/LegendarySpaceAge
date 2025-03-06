b99ad540ebfa69a69aeb47646b9facd1a368d5d3
Stephen Barnes <stephen6174@gmail.com>
2025-03-06 13:22:46 +0200

    Minor, scrap recycling

--- data/fulgora/scrap.lua
+++ data/fulgora/scrap.lua
@@ -1,15 +1,14 @@
--- Change scrap recycling outputs.
+-- Change scrap recycling outputs. Note these are actually shown in this order in the recipe.
 local scrapRecyclingResults = {
 	{ "rusty-iron-gear-wheel", 1, probability = 0.10,   show_details_in_recipe_tooltip = false },
 	{ "carbon",                1, probability = 0.10,   show_details_in_recipe_tooltip = false },
-		-- Valuable for filters.
+		-- Needed for filters.
 	{ "battery",               1, probability = 0.10,   show_details_in_recipe_tooltip = false },
 		-- Needed for sulfuric acid.
+	{ "plastic-bar",           1, probability = 0.10,   show_details_in_recipe_tooltip = false },
 
-	{ "low-density-structure", 1, probability = 0.05,   show_details_in_recipe_tooltip = false },
 	{ "advanced-circuit",      1, probability = 0.05,   show_details_in_recipe_tooltip = false },
 		-- No blue circuits, only reds - so you have to get circuit boards from the reds and spend stone and sulfuric acid to make blues.
-	{ "electric-engine-unit",  1, probability = 0.02,   show_details_in_recipe_tooltip = false },
 
 	{ "concrete",              1, probability = 0.05,   show_details_in_recipe_tooltip = false },
 
@@ -17,10 +16,17 @@ local scrapRecyclingResults = {
 
 	{ "copper-cable",          1, probability = 0.05,   show_details_in_recipe_tooltip = false },
 	{ "rubber",                1, probability = 0.05,   show_details_in_recipe_tooltip = false },
-	{ "holmium-battery",       1, probability = 0.0005, show_details_in_recipe_tooltip = false },
-	{ "holmium-ore",           1, probability = 0.005,  show_details_in_recipe_tooltip = false },
+
+	{ "low-density-structure", 1, probability = 0.02,   show_details_in_recipe_tooltip = false },
+	{ "electric-engine-unit",  1, probability = 0.02,   show_details_in_recipe_tooltip = false },
 
 	{ "ice",                   1, probability = 0.01,   show_details_in_recipe_tooltip = false },
+
+	{ "holmium-ore",           1, probability = 0.005,  show_details_in_recipe_tooltip = false },
+	{ "holmium-battery",       1, probability = 0.0005, show_details_in_recipe_tooltip = false },
+
+	{ "sulfuric-acid",         1, show_details_in_recipe_tooltip = false },
+		-- You can still do the recipe manually, this fluid product just disappears.
 
 	-- TODO stuff to add maybe: energy-shield-equipment, mechanism, electric-engine-unit, sensors, LDS, panels, wiring, frames, structures, 
 
