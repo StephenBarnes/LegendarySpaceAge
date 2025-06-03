cb6cbb64c3155bcca8381cff54b0c32f0f0889b6
Stephen Barnes <stephen6174@gmail.com>
2025-06-03 02:52:46 +0200

    Add lye, quicklime, slaked lime

--- data/pre-space/bases.lua
+++ data/pre-space/bases.lua
@@ -17,3 +17,29 @@ Icon.set(alkaliAsh, "LSA/bases/alkali-ash/1")
 Icon.variants(alkaliAsh, "LSA/bases/alkali-ash/%", 3)
 alkaliAsh.stack_size = 100 -- Increase 50->100 vs stone and ores.
 extend{alkaliAsh}
+
+-- Create slaked lime item.
+local slakedLime = copy(alkaliAsh)
+slakedLime.name = "slaked-lime"
+Icon.set(slakedLime, "LSA/bases/slaked-lime/item")
+Icon.variants(slakedLime, "LSA/bases/slaked-lime/%", 3)
+slakedLime.stack_size = 100
+extend{slakedLime}
+
+-- Create quicklime item.
+local quicklime = copy(alkaliAsh)
+quicklime.name = "quicklime"
+Icon.set(quicklime, "LSA/bases/quicklime/3") -- It's recolored calcite icons, so using different variant from calcite's main variant.
+Icon.variants(quicklime, "LSA/bases/quicklime/%", 4)
+quicklime.stack_size = 100
+extend{quicklime}
+
+-- Create lye fluid.
+local lye = copy(FLUID.electrolyte)
+lye.name = "lye"
+Icon.set(lye, "LSA/bases/lye")
+local lyeColor = {0.518, 0.196, 0.459}
+lye.base_color = lyeColor
+lye.flow_color = {1, 1, 1}
+lye.visualization_color = lyeColor
+extend{lye}
